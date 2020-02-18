8703e63115924cf8bff5eb2a7671c6720b360076
Alexander <46690347+zanchess@users.noreply.github.com>
2020-02-18 23:07:46 +0300

    feat: end caut cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
 module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) { 
+    let row = matrix[i];
+    for (let j = 0; j < row.length; j++) { 
+      if (row[j] === "^^") {
+        count++;
+      }
+    } 
+  }
+  return count;
 };
