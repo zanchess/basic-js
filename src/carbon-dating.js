dc8f89e21cf3c78ca638acdcab8b79d3430a7a43
Alexander <46690347+zanchess@users.noreply.github.com>
2020-02-18 23:21:48 +0300

    feat: end carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  if (typeof(sampleActivity) !== "string" || typeof(+sampleActivity) !== "number" || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY || /[a-zA-Z]/.test(sampleActivity) ) {
+    return false;
+  } else{
+    let age = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+    return age;
+  }
 };
