d61cf7afed617be44b3f152d1f3fff53d45895af
Alexander <46690347+zanchess@users.noreply.github.com>
2020-02-26 20:57:30 +0300

    feat: end recursive depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,12 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    calculateDepth(arr, count=1, results = []) {
+        results.push(count);
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                this.calculateDepth(arr[i], count+1, results)
+            }
+        }
+        return results.sort((a, b) => b - a)[0];
     }
 };
