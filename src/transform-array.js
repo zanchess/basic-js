b043c9b2f0827edb44ce36d8f3f1420c07e9d45d
Alexander <46690347+zanchess@users.noreply.github.com>
2020-02-29 19:35:14 +0300

    feat: end transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+  
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)){
+        throw new Error('Error');
+    } else{
+    let res = [];
+    for (let i=0; i<arr.length; i++){
+        switch (arr[i]) {
+          case '--discard-prev':
+            if(i - 1 >= 0) res.pop();
+            break;
+          case '--discard-next':
+            if (i + 1 <= arr.length - 1) i++;
+            break;
+         case '--double-prev':
+          if (i - 1 >= 0) res.push(arr[i-1]);
+            break;
+          case '--double-next':
+            if (i + 1 <= arr.length - 1) res.push(arr[i + 1]);
+            break;
+          default: res.push(arr[i]);
+      }
+    }
+    return res;
+}
+  };
