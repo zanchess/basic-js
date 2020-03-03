52b7ac3218175c80534482fd3b67116663c52b8f
Alexander <46690347+zanchess@users.noreply.github.com>
2020-03-03 21:54:50 +0300

    feat: end what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == undefined) {
+    return  'Unable to determine the time of year!';
+  }
+
+  if (date.hasOwnProperty('getMonth')) {
+    throw new Error('Error');
+  }
+
+  let month = date.getMonth();
+  switch (true) {
+    case month >= 11 || (month >= 0 && month < 2):
+      return "winter";
+      break;
+    case month >= 2 &&  month <= 4:
+        return "spring";
+        break;
+    case month >= 5 &&  month <= 7:
+        return "summer";
+        break;
+    case month >= 8 &&  month <= 10:
+        return "autumn";
+        break;
+  }
+
 };
