9754688c25d907eedad1068548b6413e50a8d4dc
Alexander <46690347+zanchess@users.noreply.github.com>
2020-03-08 22:25:59 +0300

    fix: delete commet in args

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
  	if (Array.isArray(members)) {
 		let arrMembers = members.filter(member => typeof(member) === "string" && member.trim()).map(member => member.trim()).map(member => member[0].toUpperCase()).sort().join('');
 		return arrMembers;
